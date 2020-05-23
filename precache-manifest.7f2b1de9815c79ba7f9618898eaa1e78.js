b28e1a28a76786c1fbb12a6a403efdc9ad4a2a3f
schand143 <shaikhchand724@gmail.com>
2020-05-23 22:29:39 +0530

    Updates

---
+++ precache-manifest.7f2b1de9815c79ba7f9618898eaa1e78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc46aee7ac630922a403412d8dbb85a",
+    "url": "/iTuneSearchApp/index.html"
+  },
+  {
+    "revision": "7cac6181e189174a1b4e",
+    "url": "/iTuneSearchApp/static/css/main.580bee08.chunk.css"
+  },
+  {
+    "revision": "c8c466e615f4dc6ae6c0",
+    "url": "/iTuneSearchApp/static/js/2.bb44baeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iTuneSearchApp/static/js/2.bb44baeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cac6181e189174a1b4e",
+    "url": "/iTuneSearchApp/static/js/main.baf10adf.chunk.js"
+  },
+  {
+    "revision": "ccfae5b658d9b770b34a",
+    "url": "/iTuneSearchApp/static/js/runtime-main.6636830c.js"
+  },
+  {
+    "revision": "67b3520b61a8f055b20b22fb920e5c17",
+    "url": "/iTuneSearchApp/static/media/pause-play-button.67b3520b.svg"
+  }
+]);
